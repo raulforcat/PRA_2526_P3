710c7b5bea239f8b51e9115bf766100ead00495d
raulforcat <raulforcat@gmail.com>
2026-01-06 19:23:21 +0100

    TableEntry.h actualizado

--- TableEntry.h
+++ TableEntry.h
@@ -22,6 +22,12 @@ class TableEntry {
 		friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
 			return out << "(" << te.key << "," << te.value << ")";
 		}
+    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key < te2.key;
+    }
+    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key > te2.key;
+    }
 };
 
 #endif
